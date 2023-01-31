7be71c016c417ff75b6b81b995010d2fdd85256d
Dmitrii Kilishek <mentalclear@gmail.com>
2023-01-30 22:19:32 -0500

    Some start work done

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,12 +5,28 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private WebDriver driver;
+    private By whatSearchField = By.id("text-input-what");
+    private By whereSearchField = By.id("text-input-where");
+    private By findJobsButton = By.cssSelector("button.yosegi-InlineWhatWhere-primaryButton");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
+
     private void clickLink(String linkText) {
         driver.findElement(By.linkText(linkText)).click();
+    }
+
+    public void populateWhatField(String searchTerm) {
+        driver.findElement(whatSearchField).sendKeys(searchTerm);
+    }
+    public void populateWhereField(String searchTerm) {
+        driver.findElement(whereSearchField).sendKeys(searchTerm);
+    }
+
+    public SearchResultsPage clickFindJobs() {
+        driver.findElement(findJobsButton).click();
+        return new SearchResultsPage(driver);
     }
 
     public AlertsPage clickJavascriptAlerts() {
---
+++ src/main/java/pages/SearchResultsPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SearchResultsPage {
+    private WebDriver driver;
+
+    public SearchResultsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
