759541b692f35fdc2d72056a21f2d01bf912c13d
Dmitrii Kilishek <mentalclear@gmail.com>
2023-03-07 13:36:08 -0500

    Add filter selectors

--- src/main/java/pages/AllSearchResultPage.java
+++ src/main/java/pages/AllSearchResultPage.java
@@ -1,11 +1,35 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllSearchResultPage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public AllSearchResultPage(WebDriver driver) {
         this.driver = driver;
+    }
+    By filterJobType = By.id("filter_jobType");
+    By fullTimeOption = By.xpath("//button[@value='fulltime']");
+    By fullTimeSelectedOption = By.xpath("//div[@data-test='JOBTYPE']//span[text()='Full-time']");
+    By filterPostedTime = By.id("filter_fromAge");
+    By postedTime2WeeksOption = By.xpath("//button[@value='14']");
+    By twoWeeksSelectedOption = By.xpath("//div[@data-test='DATEPOSTED']//span[text()='Last 2 Weeks']");
+
+    public void setJobTypeFilterFullTime(){
+        setJobFilter(filterJobType, fullTimeOption, fullTimeSelectedOption);
+    }
+    public void setJobPostedTime2Weeks(){
+        setJobFilter(filterPostedTime, postedTime2WeeksOption, twoWeeksSelectedOption);
+    }
+    private void setJobFilter(By selector, By option, By result){
+        wait = new WebDriverWait(driver, 5);
+        driver.findElement(selector).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(option));
+        driver.findElement(option).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(result));
     }
 }
--- src/test/java/glassdoor/workflow/GlassDoorWorkflow.java
+++ src/test/java/glassdoor/workflow/GlassDoorWorkflow.java
@@ -2,25 +2,32 @@ package glassdoor.workflow;
 
 import base.BaseTestsGlassDoor;
 import org.testng.annotations.Test;
+import pages.SignedInPageOnGlassDoor;
 
 import static org.testng.Assert.*;
 
 public class GlassDoorWorkflow extends BaseTestsGlassDoor {
+    public static SignedInPageOnGlassDoor signedInPageOnGlassDoor;
+
     @Test
     public void searchForQALocalJobs() {
-        homePageOnGlassDoor.populateEmailField("testmeup007@gmail.com");
-        homePageOnGlassDoor.clickEmailSubmitButton();
-        homePageOnGlassDoor.populatePasswordField("VerySecretPA$$word2023");
-        var signedInPageOnGlassDoor = homePageOnGlassDoor.clickSignInButton();
-        assertEquals(signedInPageOnGlassDoor.getPageTitle(),
-                "Glassdoor Job Search | Find the job that fits your life");
+        openGlassDoorAndSignIn();
 
         String searchTerm = "Software QA engineer";
         String searchLocation = "Virginia, US";
-
         signedInPageOnGlassDoor.searchForJobAndLocation(searchTerm, searchLocation);
         var bufferedResultsPage = signedInPageOnGlassDoor.clickSubmitSearch();
         var allSearchResultsPage = bufferedResultsPage.clickSeeAllJobsLink();
-        System.out.println(allSearchResultsPage);
+        allSearchResultsPage.setJobTypeFilterFullTime();
+        allSearchResultsPage.setJobPostedTime2Weeks();
+    }
+
+    private void openGlassDoorAndSignIn() {
+        homePageOnGlassDoor.populateEmailField("testmeup007@gmail.com");
+        homePageOnGlassDoor.clickEmailSubmitButton();
+        homePageOnGlassDoor.populatePasswordField("VerySecretPA$$word2023");
+        signedInPageOnGlassDoor = homePageOnGlassDoor.clickSignInButton();
+        assertEquals(signedInPageOnGlassDoor.getPageTitle(),
+                "Glassdoor Job Search | Find the job that fits your life");
     }
 }
